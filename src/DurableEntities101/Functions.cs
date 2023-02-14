d5301b1823e658708e14811d4748fe2f3180bb09
John Downs <john@johndowns.co.nz>
2023-02-14 14:58:55 +1300

    Use Event Grid trigger

--- src/DurableEntities101/Functions.cs
+++ src/DurableEntities101/Functions.cs
@@ -1,35 +1,33 @@
-using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Azure.Messaging.EventGrid;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
 using DurableEntities101.Entities;
 
 namespace DurableEntities101
 {
     public static class Functions
     {
-        // Event Grid Functions
+        const string BlobSubjectFolderNameRegex = "\\/blobServices\\/default\\/containers\\/.+\\/blobs\\/(.+)\\/(.+)";
 
-        [FunctionName("FileAddedToFolder")] // TODO make this into an Event Grid trigger
-        public static async Task<IActionResult> Run( 
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("FileAddedToFolder")]
+        public static async Task FileAddedToFolder(
+            [EventGridTrigger] EventGridEvent eventGridEvent,
             [DurableClient] IDurableEntityClient entityClient,
             ILogger log)
         {
             log.LogInformation("Event Grid trigger received notification of file added to folder.");
 
-            var folderName = "my-folder4";
-            var dateTimeUtc = DateTime.UtcNow;
-
+            // Get the name of the blob folder from the Event Grid message.
+            var match = Regex.Match(eventGridEvent.Subject, BlobSubjectFolderNameRegex);
+            var folderName = match.Groups[1].Value.Replace("/", "_");
+            
             // Signal the entity to tell it that a new blob has been added to the folder.
             var entityId = new EntityId(nameof(BlobFolderEntity), folderName);
-            await entityClient.SignalEntityAsync(entityId, nameof(BlobFolderEntity.FileUploaded), dateTimeUtc);
-
-            return new OkResult();
+            await entityClient.SignalEntityAsync(entityId, nameof(BlobFolderEntity.FileUploaded), eventGridEvent.EventTime.DateTime);
         }
     }
 }
